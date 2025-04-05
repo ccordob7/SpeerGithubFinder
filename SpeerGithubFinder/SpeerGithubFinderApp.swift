a7bc28223156c349887337e20019c59c47d9a33f
Camilo <camilocordoba31@gmail.com>
2025-04-05 13:32:48 -0400

    Initial Commit

---
+++ SpeerGithubFinder/SpeerGithubFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeerGithubFinderApp.swift
+//  SpeerGithubFinder
+//
+//  Created by Camilo on 2025-04-05.
+//
+
+import SwiftUI
+
+@main
+struct SpeerGithubFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeerGithubFinderTests/SpeerGithubFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeerGithubFinderTests.swift
+//  SpeerGithubFinderTests
+//
+//  Created by Camilo on 2025-04-05.
+//
+
+import Testing
+@testable import SpeerGithubFinder
+
+struct SpeerGithubFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
